5867fb243f23f70c6ee74bc0d5b88be0bed3b3fd
Oscar024 <or.carvajal.ortiz@gmail.com>
2017-08-28 16:16:59 -0700

    cambios

---
+++ build/default/production/cny70.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/cny70.d  \
+ build/default/production/cny70.p1:  \
+ cny70.c  \
+cny70.h 
---
+++ cny70.c
@@ -0,0 +1,18 @@
+#include "cny70.h"
+
+int getCNY70Value(void)
+{
+    if(RB0 == 1 && RB1 == 1 && RB2 == 0 && RB3 == 1 && RB4 == 1)
+        return 0;
+    else if(RB0 == 1 && RB1 == 0 && RB2 == 1 && RB3 == 1 && RB4 == 1)
+        return -1;
+    else if(RB0 == 0 && RB1 == 1 && RB2 == 1 && RB3 == 1 && RB4 == 1)
+        return -2;
+    else if(RB0 == 1 && RB1 == 1 && RB2 == 1 && RB3 == 0 && RB4 == 1)
+        return 1;
+    else if(RB0 == 1 && RB1 == 1 && RB2 == 1 && RB3 == 1 && RB4 == 0)
+        return 2;
+    else
+        return 0;
+}
+
