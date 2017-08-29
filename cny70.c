1fe6f9beb4a3c351f3e12f8b562b97fad86d51d6
Oscar024 <or.carvajal.ortiz@gmail.com>
2017-08-29 13:18:37 -0700

    cambios

--- cny70.c
+++ cny70.c
@@ -12,6 +12,14 @@ int getCNY70Value(void)
         return 1;
     else if(RB0 == 1 && RB1 == 1 && RB2 == 1 && RB3 == 1 && RB4 == 0)
         return 2;
+    else if(RB0 == 1 && RB1 == 1 && RB2 == 1 && RB3 == 0 && RB4 == 0)
+        return 2;
+    else if(RB0 == 0 && RB1 == 0 && RB2 == 1 && RB3 == 1 && RB4 == 1)
+        return -2;
+    else if(RB0 == 1 && RB1 == 0 && RB2 == 0 && RB3 == 1 && RB4 == 1)
+        return -1;
+    else if(RB0 == 1 && RB1 == 1 && RB2 == 0 && RB3 == 0 && RB4 == 1)
+        return 1;
     else
         return 0;
 }
